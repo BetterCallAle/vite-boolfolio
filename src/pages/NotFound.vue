ce35c5e7c724487004308482558ba0c03cb8c6a7
BetterCallAle <dap22091998@gmail.com>
2023-01-20 14:48:44 +0100

    Add: 404 not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+    name: 'NotFound'
+}
+</script>
+
+<template>
+    <div class="container py-5">
+        <h1 class="text-center">404 Pagina non trovata</h1>
+        <p class="text-center">La pagina che cerchi: {{ $route.path }} non è stata trovata</p>
+        <div class="text-center">
+            <router-link :to="{ name: 'home' }" class="btn btn-primary">Torna alla pagina Home</router-link>
+        </div>
+    </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from './pages/HomePage.vue';
 import AboutUs from './pages/AboutUs.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -14,6 +15,11 @@ const router = createRouter({
             path: '/about-us',
             name: 'about',
             component: AboutUs
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'notFound',
+            component: NotFound
         }
     ]
 })

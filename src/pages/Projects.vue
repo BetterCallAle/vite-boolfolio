83a1565ddeadeb5a0e3fbfdd0f5cf8b40cde44a6
BetterCallAle <dap22091998@gmail.com>
2023-01-20 15:48:06 +0100

    Add: Loader in Projects page

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,12 +1,14 @@
 <script>
 import {store} from '../store';
 import ProjectCard from '../components/ProjectCard.vue';
+import AppLoader from '../components/AppLoader.vue';
 import axios from 'axios';
 
 export default{
     name: "Projects",
     components: {
-        ProjectCard 
+        ProjectCard,
+        AppLoader
     },
     data() {
         return {
@@ -14,7 +16,8 @@ export default{
             currentPage: 1,
             lastPage: null,
             projects: [],
-            totalProjects: null
+            totalProjects: null,
+            loading: false
         }
     },
     created(){
@@ -22,6 +25,9 @@ export default{
     },
     methods:{
         getProjects(page){
+            //Set the loader true
+            this.loading = true;
+
             //Set the page params
             const option = {
                 params: {
@@ -29,11 +35,13 @@ export default{
                 }
             }
 
+            // Do the axios call
             axios.get(`${this.store.apiUrl}/api/projects`, option).then((resp) =>{
                 this.currentPage = resp.data.results.current_page;
                 this.lastPage = resp.data.results.last_page;
                 this.totalProjects = resp.data.results.total;
                 this.projects = resp.data.results.data;
+                this.loading = false
             })
         }
     }
@@ -46,47 +54,52 @@ export default{
 
             <h1 class="text-center mb-5">Tutti i nostri progetti</h1>
 
-            <p class="text-end text-secondary">Trovati <span class="fw-bold text-black">{{ totalProjects }}</span> progetti</p>
+            <AppLoader v-if="loading"/>
 
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-                <ProjectCard v-for="project in projects" :key="project.id" :project="project" :apiUrl="store.apiUrl" />
-           </div>
+            <div v-else class="project-wrapper">
+                <p class="text-end text-secondary">Trovati <span class="fw-bold text-black">{{ totalProjects }}</span> progetti</p>
+    
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+                    <ProjectCard v-for="project in projects" :key="project.id" :project="project" :apiUrl="store.apiUrl" />
+               </div>
+    
+               <!-- Pagination -->
+               <div class="pagination d-flex justify-content-center mt-5">
+                    <!-- Go to first page button -->
+                    <a class="btn btn-primary me-1" :class="{'disabled' : currentPage === 1 }" @click.prevent="getProjects(1)">
+                        <i class="fa-solid fa-chevron-left"></i>
+                        <i class="fa-solid fa-chevron-left"></i>
+                    </a>
+                    <!-- /Go to first page button -->
+    
+                    <!-- Go to prev page button -->
+                    <a class="btn btn-primary" :class="{'disabled' : currentPage === 1 }" @click.prevent="getProjects(currentPage -1)">
+                        <i class="fa-solid fa-chevron-left"></i>
+                    </a>
+                    <!-- /Go to prev page button -->
+    
+                    <!-- Buttons with page numbers -->
+                    <div class="page-numbers">
+                        <a class="btn btn-primary mx-1" :class="{'disabled' : currentPage === pageNumber}" v-for="(pageNumber, index) in lastPage" :key="index" @click.prevent="getProjects(pageNumber)">{{ pageNumber }}</a>
+                    </div>
+                    <!-- /Buttons with page numbers -->
+    
+                    <!-- Go to next page button -->
+                    <a class="btn btn-primary" :class="{'disabled' : currentPage === lastPage}" @click.prevent="getProjects(currentPage +1)">
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </a>
+                    <!-- /Go to next page button -->
+    
+                    <!-- Go to last page button -->
+                    <a class="btn btn-primary ms-1" :class="{'disabled' : currentPage === lastPage }" @click.prevent="getProjects(lastPage)">
+                        <i class="fa-solid fa-chevron-right"></i>
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </a>
+                    <!-- /Go to last page button -->
+               </div>
+               <!-- /Pagination -->
+            </div>
 
-           <!-- Pagination -->
-           <div class="pagination d-flex justify-content-center mt-5">
-                <!-- Go to first page button -->
-                <a class="btn btn-primary me-1" :class="{'disabled' : currentPage === 1 }" @click.prevent="getProjects(1)">
-                    <i class="fa-solid fa-chevron-left"></i>
-                    <i class="fa-solid fa-chevron-left"></i>
-                </a>
-                <!-- /Go to first page button -->
-
-                <!-- Go to prev page button -->
-                <a class="btn btn-primary" :class="{'disabled' : currentPage === 1 }" @click.prevent="getProjects(currentPage -1)">
-                    <i class="fa-solid fa-chevron-left"></i>
-                </a>
-                <!-- /Go to prev page button -->
-
-                <!-- Buttons with page numbers -->
-                <div class="page-numbers">
-                    <a class="btn btn-primary mx-1" :class="{'disabled' : currentPage === pageNumber}" v-for="(pageNumber, index) in lastPage" :key="index" @click.prevent="getProjects(pageNumber)">{{ pageNumber }}</a>
-                </div>
-                <!-- /Buttons with page numbers -->
-
-                <!-- Go to next page button -->
-                <a class="btn btn-primary" :class="{'disabled' : currentPage === lastPage}" @click.prevent="getProjects(currentPage +1)">
-                    <i class="fa-solid fa-chevron-right"></i>
-                </a>
-                <!-- /Go to next page button -->
-
-                <!-- Go to last page button -->
-                <a class="btn btn-primary ms-1" :class="{'disabled' : currentPage === lastPage }" @click.prevent="getProjects(lastPage)">
-                    <i class="fa-solid fa-chevron-right"></i>
-                    <i class="fa-solid fa-chevron-right"></i>
-                </a>
-                <!-- /Go to last page button -->
-           </div>
-           <!-- /Pagination -->
 
         </div>
     </section>
